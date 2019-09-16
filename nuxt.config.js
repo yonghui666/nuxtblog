6755b8487faeb249da26df74db255f842b5c0970
李小白 <65048448@qq.com>
2019-09-16 16:04:39 +0800

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+
+module.exports = {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#87CEEB' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    '~assets/css/reset.scss',
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui',
+    {
+      src:'~/plugins/wangeditor',
+      ssr:false
+    },
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+  ],
+  axios: {
+    proxy: true
+  },
+  proxy: {    //能解决前端跨域问题
+    '/api': {
+      target: 'http://127.0.0.1:3333',
+      pathRewrite: {
+        '^/api' : '/'
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^element-ui/],
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ plugins/wangeditor.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import wangeditor from 'wangeditor';
+Vue.use('wangeditor')
